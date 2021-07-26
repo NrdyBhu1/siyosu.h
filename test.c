e887877989a94d30e50071dd853541b0695ed1eb
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-07-26 10:14:42 +0530

    add test.c

---
+++ test.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define SIYOSU_IMPLEMENTATION
+#define SIYOSU_NO_DEFAULT_VEC
+#define SIYOSU_NO_DEFAULT_HT
+#include "siyosu.h"
+
+void pr(int d) {
+  printf("%d\n", d);
+}
+
+int main() {
+  VECTOR_DEF(int);
+  vec_int vec;
+  vec_int_init(&vec, 10);
+  for(int i = 0; i < 10; i++) {
+    vec_int_push(&vec, i);
+  }
+  for(int i = 0; i < 10; i++) {
+    pr(vec.arr[i]);
+  }
+  return 0;
+}
